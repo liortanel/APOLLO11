a5628f7dba5a6714ea7100f2a46858209ddaf7fd
LIOR TANEL <49123993@ort>
2025-05-29 15:44:45 -0300

    audioAlien y agregamos minijuego Marte

--- obj/Debug/net8.0/APOLLO11.AssemblyInfo.cs
+++ obj/Debug/net8.0/APOLLO11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APOLLO11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b292113acd36da59c2bb1982681298a18a3d433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56709fc9b995d2efe4d50ebe747fb2ec39f3c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("APOLLO11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APOLLO11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
